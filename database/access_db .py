65e0830fe28ac953830b3b8b6f96c19a84c37e6d
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-12-03 21:37:34 +0530

    Update access_db .py

--- database/access_db .py
+++ database/access_db .py
@@ -1,6 +1,6 @@
 # (c) @AbirHasan2005
 
 from config import Config
-from helpers.database import Database
+from database.database import Database
 
 db = Database(Config.DATABASE_URL, Config.SESSION_NAME)
