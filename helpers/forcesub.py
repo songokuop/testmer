041b7bc4d27f49d0d4b5a9252792c586c28c9542
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-12-03 19:46:29 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -7,19 +7,19 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
-async def ForceSub(bot: Client, cmd: Message):
+async def ForceSub(c: Client, cmd: Message):
     try:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+        invite_link = await c.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+        invite_link = await c.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
     except Exception as err:
         print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
         return 200
     try:
-        user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cmd.from_user.id)
+        user = await c.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cmd.from_user.id)
         if user.status == "kicked":
-            await bot.send_message(
+            await c.send_message(
                 chat_id=cmd.from_user.id,
                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
                 parse_mode="markdown",
@@ -27,7 +27,7 @@ async def ForceSub(bot: Client, cmd: Message):
             )
             return 400
     except UserNotParticipant:
-        await bot.send_message(
+        await c.send_message(
             chat_id=cmd.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
             reply_markup=InlineKeyboardMarkup(
@@ -44,7 +44,7 @@ async def ForceSub(bot: Client, cmd: Message):
         )
         return 400
     except Exception:
-        await bot.send_message(
+        await c.send_message(
             chat_id=cmd.from_user.id,
             text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
             parse_mode="markdown",
