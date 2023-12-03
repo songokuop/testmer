b0d85baf8f62030aa1d42d6a789c4c37358843eb
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-12-03 21:21:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,9 +43,9 @@ from __init__ import (
     replyDB,
 )
 from config import Config
-from helpers.databases.access_db import db
+from helpers.database.access_db import db
 from helpers.forcesub import ForceSub
-from helpers.databases.add_user import AddUserToDatabase
+from helpers.database.add_user import AddUserToDatabase
 from helpers.utils import UserSettings, get_readable_file_size, get_readable_time
 
 botStartTime = time.time()
