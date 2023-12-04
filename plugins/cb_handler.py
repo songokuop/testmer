aac40c3226c1f688687ee0e46c87d419b71cf7e2
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-12-04 20:05:37 +0530

    Update cb_handler.py

--- plugins/cb_handler.py
+++ plugins/cb_handler.py
@@ -92,8 +92,8 @@ async def callback_handler(c: Client, cb: CallbackQuery):
     elif "refreshFsub" in cb.data:
         if Config.UPDATES_CHANNEL:
             try:
-                user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cb.message.chat.id)
-                if user.banned:
+                user = await c.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cb.message.chat.id)
+                if user.status =="kicked":
                     await cb.message.edit(
                         text="Sorry Sir, You are Banned to use me. Contact my Admin.",
                         parse_mode="Markdown",
@@ -102,10 +102,10 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                     return
             except UserNotParticipant:
                 try:
-                    invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+                    invite_link = await c.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
-                    invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+                    invite_link = await c.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
                 await cb.message.edit(
                     text="**You Still Didn't Join ☹️, Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
                     reply_markup=InlineKeyboardMarkup(
@@ -127,6 +127,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
                     disable_web_page_preview=True
                 )
                 return
+                
         await cb.message.edit(
             text=Config.START_TEXT,
             parse_mode="Markdown",
