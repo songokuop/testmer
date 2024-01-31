85679d514e4919b16318d5a96383a80d0fabd2bb
Darkksoul <139141380+Darkksoul@users.noreply.github.com>
2024-01-31 13:26:34 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -22,7 +22,7 @@ async def ForceSub(c: Client, m: Message):
         if user.status == "kicked":
             await c.send_message(
                 chat_id=m.from_user.id,
-                text="Sᴏʀʀʏ sɪʀ, ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴍʏ ᴀᴅᴍɪɴ .",
+                text="Sᴏʀʀʏ sɪʀ, ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴍʏ ᴀᴅᴍɪɴ @DevilServers .",
                 disable_web_page_preview=True,
                 parse_mode="Markdown",
                 
